2df5201696008e64f1dd7e0480951d06c7ce8b21
tm17013 <tm17013@ues.edu.sv>
2021-05-31 10:09:20 -0600

    Creacion de Controllers, Models y Seeder

---
+++ app/Http/Controllers/VentaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Compras;
+use App\Models\Productos;
+
+class VentaController extends Controller
+{
+    //
+}
