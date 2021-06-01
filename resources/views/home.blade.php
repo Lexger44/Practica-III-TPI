8a7e93e5d0c26687c99fad83cf0040f5799a2f4f
mp17001-prn335-cicloII-2019 <melissa.martinez@oportunidades.org.sv>
2021-06-01 15:02:02 -0600

    Se ajustan los seeders para las migraciones. Primer borrador de interfaz de la venta

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Compras') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,27 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Seleccione el producto que desea comprar') }}
+                    
+                    <div class="input-group mb-3">
+                        <label class="input-group-text" for="inputGroupSelect01">Producto</label>
+                            <select class="form-select" id="inputGroupSelect01">
+                                  <option selected>Seleccionar un producto</option>
+                                  <option value="1">Guaro</option>
+                                  <option value="2">Mariguana</option>
+                                  <option value="3">Tequila</option>
+                             </select>
+                    </div>
+                    <div class="input-group mb-3">
+                        <label class="input-group-text" for="inputGroupSelect01">Unidades</label>
+                            <select class="form-select" id="inputGroupSelect01">
+                                  <option selected>Unidades</option>
+                                  <option value="1">1</option>
+                                  <option value="2">2</option>
+                                  <option value="3">3</option>
+                             </select>
+                    </div>
+
                 </div>
             </div>
         </div>
